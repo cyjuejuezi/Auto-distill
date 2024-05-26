ea16c9a1bdc9fe617176b3e1ff5d515fa2cfac0e
Yang Cao <91747139+cyjuejuezi@users.noreply.github.com>
2024-05-26 16:00:56 +0800

    distilling step by step

---
+++ train_utils.py
@@ -0,0 +1,112 @@
+# Copyright 2023 The Distilling-step-by-step authors
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import shutil
+import logging
+import csv
+from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer
+from transformers import T5ForConditionalGeneration
+from transformers import DataCollatorForSeq2Seq
+from transformers.trainer_utils import set_seed
+from model_utils import TaskPrefixDataCollator, TaskPrefixTrainer
+
+def get_config_dir(args):
+    return f'{args.dataset}/{args.from_pretrained.split("/")[1]}/{args.model_type}/{args.llm}/{args.subsample}/{args.label_type}/{args.alpha}/{args.max_input_length}/{args.grad_steps*args.batch_size}/{args.optimizer_name}/{args.lr}'
+
+def train_and_evaluate(args, run, tokenizer, tokenized_datasets, compute_metrics):
+    set_seed(run)
+
+    model = T5ForConditionalGeneration.from_pretrained(args.from_pretrained)
+    if args.parallelize:
+        model.parallelize()
+
+    config_dir = get_config_dir(args)
+    output_dir = f'ckpts/{config_dir}/{run}'  # for model ckpts
+    logging_dir = f'logs/{config_dir}/{run}'  # for training logs
+
+    if args.no_log:
+        logging_strategy = 'no'
+        logging_dir = None
+    else:
+        logging_strategy = 'steps'
+
+    # Clear output dir if already exists
+    if os.path.exists(output_dir):
+        logging.info('Found existing ckpt directory. Deleted the old directory for the latest run.')
+        shutil.rmtree(output_dir)
+
+    training_args = Seq2SeqTrainingArguments(
+        output_dir=output_dir,
+        remove_unused_columns=False,
+        evaluation_strategy='steps',
+        eval_steps=args.eval_steps,
+        save_strategy='no',
+        save_steps=args.eval_steps,
+        logging_dir=logging_dir,
+        logging_strategy=logging_strategy,
+        logging_steps=args.eval_steps,
+        max_steps=args.max_steps,
+        learning_rate=args.lr,
+        gradient_accumulation_steps=args.grad_steps,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        predict_with_generate=True,
+        seed=run,
+        local_rank=args.local_rank,
+        bf16=args.bf16,
+        generation_max_length=args.gen_max_len,
+        prediction_loss_only=False,
+    )
+
+    if args.model_type == 'task_prefix':
+        data_collator = TaskPrefixDataCollator(tokenizer=tokenizer, model=model)
+    elif args.model_type == 'standard':
+        data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+    else:
+        raise ValueError
+
+    trainer_kwargs = {
+        'model': model,
+        'args': training_args,
+        'train_dataset': tokenized_datasets["train"],
+        'eval_dataset': tokenized_datasets["valid"],
+        'data_collator': data_collator,
+        'tokenizer': tokenizer,
+        'compute_metrics': compute_metrics,
+    }
+
+    trainer = Seq2SeqTrainer(**trainer_kwargs) if args.model_type == 'standard' else TaskPrefixTrainer(**trainer_kwargs)
+
+    # Start training
+    trainer.train()
+
+    # After training, log desired metrics to CSV
+    csv_file_path = 'loss_accuracy.csv'
+    # After training, log desired metrics to CSV
+    with open(csv_file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Epoch', 'Training Loss', 'Validation Loss', 'Validation Accuracy'])
+
+        # 确保这里引用的是验证集的评估结果
+        for log in trainer.state.log_history:
+            if 'loss' in log:  # Training log
+                epoch = log.get('epoch', 'N/A')
+                training_loss = log['loss']
+            if 'eval_loss' in log and 'epoch' in log:  # Validation log
+                validation_loss = log['eval_loss']
+                validation_accuracy = log.get('eval_accuracy', "N/A")
+                writer.writerow([epoch, training_loss, validation_loss, validation_accuracy])
+
